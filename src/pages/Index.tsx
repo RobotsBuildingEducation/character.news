8fad8e6f32436154ad1aaec78d22f2b89dfe7cee
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2025-06-26 14:39:42 -0600

    store

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -17,7 +17,10 @@ const Index = () => {
   const { balance, deposit, zap } = useNutsack();
 
   return (
-    <div className="min-h-screen flex flex-col items-center gap-6 p-6">
+    <div
+      className="min-h-screen flex flex-col items-center gap-6 p-6"
+      style={{ marginTop: 56 }}
+    >
       <h1 className="text-2xl font-bold">Character News</h1>
       <Link to="/about" className="text-sm">
         About
