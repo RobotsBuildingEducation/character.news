df99fd2b461c5e4c5c3610415601a04eab5e4147
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2025-07-05 20:10:57 -0600

    store

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fk9t3ref33"
+    "revision": "0.as6e7dosgoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
@@ -9,7 +10,47 @@ export default defineConfig(() => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6100000, // Set to 4MB or any higher value
+      },
+      manifest: {
+        name: "Character News",
+        short_name: "Character News",
+        start_url: "./",
+        display: "standalone",
+        theme_color: "#d2d2d2",
+        background_color: "#d2d2d2",
+        description: "PWA install handler package for Character news",
+        icons: [
+          {
+            src: "https://res.cloudinary.com/dtkeyccga/image/upload/v1751074363/2_dv9kah.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "https://res.cloudinary.com/dtkeyccga/image/upload/v1751074363/2_dv9kah.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "https://res.cloudinary.com/dtkeyccga/image/upload/v1751074363/2_dv9kah.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
